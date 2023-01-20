a27479be7bf5fca0575ad7bf4a0e70ec447feddc
nkr413 <nkgithubnarkuziev108@gmail.com>
2023-01-21 00:53:31 +0500

    ADD: Main Class

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
+class Main
+{
+  w = canvas.width = window.innerWidth;
+  h = canvas.height = window.innerHeight;
+
+  constructor(ctx)
+  { this.ctx = ctx; }
+
+  draw() {
+    this.ctx.fillStyle = "green";
+    this.ctx.fillRect(10, 10, 150, 100);
+  }
+
+}
+
+
+const main = new Main(canvas.getContext("2d"));
+main.draw();
+
+window.onload = () =>
+{
+  document.getElementById("canvas").style.width = main.w;
+  document.getElementById("canvas").style.height = main.h;
+};
