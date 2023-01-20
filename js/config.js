b82269a4456210ffaf0ef8fb8850128047292fc2
nkr413 <nkgithubnarkuziev108@gmail.com>
2023-01-21 04:17:58 +0500

    add: descriptons

--- js/config.js
+++ js/config.js
@@ -1,8 +1,14 @@
 const head = {
 	color: "red",
-	w: 2,
-	h: 2,
-	km: 4,
+	w: 4, // width of block, default - 4
+	h: 4, // height of block, default - 4
+	km: 8, // distance between blocks, default - 8
 };
 
 const child = { color: "#5B5C5F" };
+
+const time = {
+	main: 50, // fps of generation, default - 50
+	int: 30, // the rate of trajectory change, default - 30
+	out: 100, // amount of distance, default - 100
+};
