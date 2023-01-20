08bb88edaf4bcbe3607167a5b16f2c0ea7a5c945
nkr413 <nkgithubnarkuziev108@gmail.com>
2023-01-21 04:05:01 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,23 +1,69 @@
 const canvas = document.getElementById("canvas");
 
+
 class Main
 {
   w = canvas.width = window.innerWidth;
   h = canvas.height = window.innerHeight;
+  x = 0;
+  y = 0;
+  side = 3;
+
 
   constructor(ctx)
   { this.ctx = ctx; }
 
-  draw() {
-    this.ctx.fillStyle = "green";
-    this.ctx.fillRect(10, 10, 150, 100);
+
+  random()
+  { this.side = Math.floor(Math.random() * 4) + 1; }
+
+
+  draw()
+  {
+    this.ctx.fillStyle = child.color;
+    this.ctx.fillRect(this.x, this.y, head.w, head.h);
+
+    switch (this.side) {
+      case 4: this.x -= head.km; break;
+      case 3: this.x += head.km; break;
+      case 2: this.y -= head.km; break;
+      case 1: this.y += head.km; break;
+    }
+
+    if (this.x < 0) this.x = 0;
+    if (this.x > this.w) this.x = this.w;
+    if (this.y < 0) this.y = 0;
+    if (this.y > this.y) this.y = this.y;
+
+    this.ctx.fillStyle = head.color;
+    this.ctx.fillRect(this.x, this.y, head.w, head.h);
   }
 
+
+  interval()
+  {
+    let int = setInterval(() => {
+      setTimeout(() => { clearInterval(int) }, 100);
+      this.draw();
+    }, 30);
+  }
+
+
+  loop()
+  {
+    setInterval(() => {
+      this.random();
+      this.interval();
+    }, 50);
+
+    //window.requestAnimationFrame(() => this.loop());
+  }
 }
 
 
 const main = new Main(canvas.getContext("2d"));
-main.draw();
+main.loop();
+
 
 window.onload = () =>
 {
